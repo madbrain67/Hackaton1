13048a9a5498d72c2051b3db0156a430bfbe4afe
David Vighi <david.vighi@gmail.com>
2019-04-18 01:31:41 +0200

    Index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,20 @@ use App\Service\Jeux;
 class HomeController extends AbstractController
 {
 
-
-
     public function index(){
 
         $jeux = new Jeux();
         $gain = $jeux->getGains();
         return $this->twig->render('home/index.html.twig', [
             'gain' => $gain,
+        ]);
+    }
+
+    public function dialogue(){
+
+
+        return $this->twig->render('dialogue/index.html.twig', [
+
         ]);
     }
 
